235418d198a47edda8e5253cfc04e56dd9ad894c
LuiggiChia <167223335+LuiggiChia@users.noreply.github.com>
2026-06-23 17:12:14 -0500

    v21 commit

--- src/extraction/scrapping_facturacion_faast.py
+++ src/extraction/scrapping_facturacion_faast.py
@@ -16,14 +16,11 @@ def inner_playwrite(
     password = faast_credentials["password"]
 
     logger.info("Credenciales - Faast cargadas correctamente")
-
     fecha_inicio = dia_de_reporte.strftime("%d/%m/%Y")
     browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
     page = context.new_page()
     try:
-        bronze_data_path = os.path.join(BASE_DIR, "data/bronze")
-        
         page.goto("https://toquea.faast.pe/")
         page.set_default_timeout(90000)
         page.get_by_role("textbox", name="Nombre de Usuario").click()
@@ -31,41 +28,35 @@ def inner_playwrite(
         page.get_by_role("textbox", name="Nombre de Usuario").press("Tab")
         page.get_by_role("textbox", name="Contraseña").fill(password)
         page.get_by_role("textbox", name="Contraseña").press("Enter")
-        # page.goto("https://toquea.faast.pe/faast-reporting/reports#/")
-        logger.info("-- Ingreso a la web")
-        page.locator('[id="2"]').get_by_text("Reportes", exact=True).click()
+        print("-- Ingreso a la web")
+        page.locator("[id=\"2\"]").get_by_text("Reportes", exact=True).click()
         page.locator("a").filter(has_text="Reportes Backoffice").click()
-        logger.info("-- Entro a reporte")
-        page.get_by_role("listitem").filter(
-            has_text="FacturacionVer Detalles"
-        ).get_by_role("button").click()
-        page.locator("iframe").content_frame.get_by_role(
-            "textbox", name="Desde"
-        ).click()
-        page.locator("iframe").content_frame.get_by_role("textbox", name="Desde").fill(
-            fecha_inicio
-        )
+        print("-- Entro a reporte")
+        page.get_by_role("listitem").filter(has_text="FacturacionVer Detalles").get_by_role("button").click()
+        page.locator("iframe").content_frame.get_by_role("textbox", name="Desde").click()
+        input_desde_hasta = dia_de_reporte.strftime("%m/%d/%Y")
+        page.locator("iframe").content_frame.get_by_role("textbox", name="Desde").fill(input_desde_hasta)
+        page.locator("iframe").content_frame.get_by_role("textbox", name="Desde").press("Tab")
         page.wait_for_timeout(10000)
-        page.locator("iframe").content_frame.get_by_role(
-            "textbox", name="Hasta"
-        ).click()
-        page.locator("iframe").content_frame.get_by_role("textbox", name="Hasta").fill(
-            fecha_inicio
-        )
+        page.locator("iframe").content_frame.get_by_role("textbox", name="Hasta").click()
+        page.locator("iframe").content_frame.get_by_role("textbox", name="Hasta").fill(input_desde_hasta)
         page.wait_for_timeout(10000)
-        # Modificación
         frame = page.frame_locator("iframe")
         frame.locator("#ReportViewerControl_ctl04_ctl00").scroll_into_view_if_needed()
         frame.locator("#ReportViewerControl_ctl04_ctl00").click(force=True)
         page.wait_for_timeout(6000)
-        logger.info("-- Se ve informe")
+        print("-- Se ve informe")
         frame.locator("#ReportViewerControl_ctl05_ctl04_ctl00_ButtonImg").click(force=True)
         with page.expect_download() as download_info:
-            frame.get_by_role("link", name="CSV (delimitado por comas)").click()
+            frame.get_by_role(
+                "link",
+                name="CSV (comma delimited)"
+            ).click()
+
         download = download_info.value
         bronze_data_path = os.path.join(BASE_DIR, "data/bronze")
         download.save_as(f'{bronze_data_path}/{dia_de_reporte.strftime("%d_%m_%Y")}.csv')
-        logger.info("-- Se guardo archivo")
+
         # ---------------------
         context.close()
         browser.close()
