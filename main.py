a712c435225843ce03aaf5d21463b7e7652ff761
LuiggiChia <167223335+LuiggiChia@users.noreply.github.com>
2026-06-23 15:52:15 -0500

    v10 commit

--- main.py
+++ main.py
@@ -3,16 +3,14 @@ import json
 
 from src.extraction.scrapping_facturacion_faast import exports_csv
 
-
 project_path = os.path.dirname(os.path.abspath(__file__))
-credentials_path = os.path.join(project_path, "config",
-                                "faast_credentials.json")
+credentials_path = os.path.join(project_path, "config", "faast_credentials.json")
 
 with open(credentials_path, "r", encoding="utf-8") as file:
     faast_credentials = json.load(file)
 usuario = faast_credentials["usuario"]
 password = faast_credentials["password"]
 
-downloads_path = os.path.join(os.environ["USERPROFILE"], "Downloads")
 
-print(downloads_path)
+if __name__ == "__main__":
+    exports_csv()
--- src/extraction/scrapping_facturacion_faast.py
+++ src/extraction/scrapping_facturacion_faast.py
@@ -1,175 +1,76 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
-import time
-import datetime
+import re
+from datetime import datetime
+from playwright.sync_api import Playwright, sync_playwright, expect
 
 
-def exports_csv(usuario: str, password: str) -> bool:
+def inner_playwrite(
+    playwright: Playwright,
+    usuario: str,
+    password: str,
+    dia_de_reporte: datetime,
+    BASE_DIR: str,
+) -> bool:
 
-    fecha_inicio = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime(
-        "%d/%m/%Y"
-    )
-    # fecha_inicio = '28/11/2025'
-
-    print("inicio")
-    chrome_options = Options()
-    chrome_options.add_argument("--headless=new")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--disable-notifications")
-    chrome_options.add_argument("--disable-popup-blocking")
-    chrome_options.add_argument("--disable-notifications")
-    chrome_options.add_argument("--window-size=1920,1080")
-    chrome_options.add_argument("--start-maximized")
-    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-    chrome_options.add_argument("--headless=new")  # Actívalo si no necesitas interfaz gráfica
-
-    # Lanzar Chrome más rápido
-    driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()), options=chrome_options
-    )
-
+    fecha_inicio = dia_de_reporte.strftime("%d/%m/%Y")
+    browser = playwright.chromium.launch(headless=True)
+    context = browser.new_context()
+    page = context.new_page()
     try:
-        # ====================
-        # LOGIN
-        # ====================
-        driver.get("https://toquea.faast.pe/faast-reporting/reports#/")
 
-        wait = WebDriverWait(driver, 20)
-
-        # Esperar inputs
-        wait.until(EC.presence_of_element_located((By.ID, "usuario")))
-
-        driver.find_element(By.ID, "usuario").send_keys(usuario)
-        driver.find_element(By.ID, "contraseña").send_keys(password)
-
-        # Clic en Ingresar
-        boton_ingresar = wait.until(
-            EC.element_to_be_clickable(
-                (By.XPATH, "//button[contains(text(),'Ingresar')]")
-            )
+        page.goto("https://toquea.faast.pe/")
+        page.set_default_timeout(90000)
+        page.get_by_role("textbox", name="Nombre de Usuario").click()
+        page.get_by_role("textbox", name="Nombre de Usuario").fill(usuario)
+        page.get_by_role("textbox", name="Nombre de Usuario").press("Tab")
+        page.get_by_role("textbox", name="Contraseña").fill(password)
+        page.get_by_role("textbox", name="Contraseña").press("Enter")
+        # page.goto("https://toquea.faast.pe/faast-reporting/reports#/")
+        print("-- Ingreso a la web")
+        page.locator('[id="2"]').get_by_text("Reportes", exact=True).click()
+        page.locator("a").filter(has_text="Reportes Backoffice").click()
+        print("-- Entro a reporte")
+        page.get_by_role("listitem").filter(
+            has_text="FacturacionVer Detalles"
+        ).get_by_role("button").click()
+        page.locator("iframe").content_frame.get_by_role(
+            "textbox", name="Desde"
+        ).click()
+        page.locator("iframe").content_frame.get_by_role("textbox", name="Desde").fill(
+            fecha_inicio
         )
-        boton_ingresar.click()
-
-        time.sleep(30)
-        # Ir manualmente al módulo de reportes (manteniendo sesión)
-        driver.get("https://toquea.faast.pe/faast-reporting/reports#/")
-
-        driver.save_screenshot("captura.png")
-        print("se tomo screen")
-
-        # ====================
-        # NAVEGAR A VOLUMEN OPERACIONAL
-        # ====================
-
-        # Esperar carga de lista de reportes
-        li_volumen = WebDriverWait(driver, 20).until(
-            EC.visibility_of_element_located(
-                (
-                    By.XPATH,
-                    "//li[contains(@class, 'list-group-item')][.//text()[contains(., 'Facturacion')]]",
-                )
-            )
+        page.wait_for_timeout(10000)
+        page.locator("iframe").content_frame.get_by_role(
+            "textbox", name="Hasta"
+        ).click()
+        page.locator("iframe").content_frame.get_by_role("textbox", name="Hasta").fill(
+            fecha_inicio
         )
-
-        # Clic en Ver Detalles
-        boton = li_volumen.find_element(
-            By.XPATH, ".//button[contains(text(), 'Ver Detalles')]"
-        )
-        driver.execute_script("arguments[0].click();", boton)
-
-        print("Accediendo al reporte Recaudacion...")
-
-        # ====================
-        # CAMBIAR A IFRAME
-        # ====================
-
-        iframe = WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located(
-                (By.XPATH, "//iframe[contains(@src, 'ReportServer')]")
-            )
-        )
-        driver.switch_to.frame(iframe)
-
-        print("Dentro del iframe del reporte.")
-
-        # ====================
-        # SETEAR FECHA DESDE (actual - 30 días)
-        # ====================
-
-        WebDriverWait(driver, 15).until(
-            EC.presence_of_element_located(
-                (By.ID, "ReportViewerControl_ctl04_ctl03_txtValue")
-            )
-        )
-
-        driver.execute_script(
-            "document.getElementById('ReportViewerControl_ctl04_ctl03_txtValue').value = arguments[0];",
-            fecha_inicio,
-        )
-
-        print(f"Fecha desde seteada a: {fecha_inicio}")
-
-        # ====================
-        # VER INFORME
-        # ====================
-
-        boton_ver_informe = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.ID, "ReportViewerControl_ctl04_ctl00"))
-        )
-        boton_ver_informe.click()
-
-        print("Clic en 'Ver informe'. Esperando generación...")
-
-        # Tiempo de espera hasta que se renderice el reporte
-        time.sleep(5)  # Puede reemplazarse con espera más inteligente
-
-        # ====================
-        # EXPORTAR A CSV
-        # ====================
-
-        # Clic en el botón de exportar
-        boton_exportar = WebDriverWait(driver, 20).until(
-            EC.element_to_be_clickable(
-                (By.ID, "ReportViewerControl_ctl05_ctl04_ctl00_ButtonLink")
-            )
-        )
-        driver.execute_script("arguments[0].click();", boton_exportar)
-
-        print("Menú de exportación desplegado.")
-
-        # Seleccionar CSV
-        opcion_csv = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable(
-                (By.XPATH, "//a[contains(text(), 'CSV (delimitado por comas)')]")
-            )
-        )
-        opcion_csv.click()
-
-        time.sleep(10)
-
-        print("Exportación completada con éxito.")
-
+        page.wait_for_timeout(10000)
+        # Modificación
+        frame = page.frame_locator("iframe")
+        frame.locator("#ReportViewerControl_ctl04_ctl00").scroll_into_view_if_needed()
+        frame.locator("#ReportViewerControl_ctl04_ctl00").click(force=True)
+        page.wait_for_timeout(6000)
+        print("-- Se ve informe")
+        with page.expect_download() as download_info:
+            frame.get_by_role("link", name="CSV (comma delimited)").click()
+        download = download_info.value
+        download.save_as(f'{BASE_DIR}/{dia_de_reporte.strftime("%d_%m_%Y")}.csv')
+        print("-- Se guardo archivo")
+        # ---------------------
+        context.close()
+        browser.close()
         return True
-
     except Exception as e:
         print(f"Error durante la exportación: {e}")
         return False
-
     finally:
-        if "driver" in locals():
-            driver.quit()
+        print("Finally")
+        # ---------------------
+        context.close()
+        browser.close()
 
 
-if __name__ == "__main__":
-    usuario = "luiggi.chia@ajegroup.com"
-    password = "m1VJQ3!8u)t*"
-
-    resultado = exports_csv(usuario, password)
+def exports_csv(usuario: str, password: str, dia_de_reporte: datetime) -> bool:
+    with sync_playwright() as playwright:
+        return inner_playwrite(playwright, usuario, password, dia_de_reporte)
