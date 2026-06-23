b61af7476a3a861eca3d4b95f212e6e53de9471e
LuiggiChia <167223335+LuiggiChia@users.noreply.github.com>
2026-06-23 16:50:03 -0500

    v17 commit

--- src/extraction/scrapping_facturacion_faast.py
+++ src/extraction/scrapping_facturacion_faast.py
@@ -77,6 +77,6 @@ def inner_playwrite(
         browser.close()
 
 
-def exports_csv(dia_de_reporte: datetime, BASE_DIR: str) -> bool:
+def exports_csv(dia_de_reporte: datetime, BASE_DIR: str, logger) -> bool:
     with sync_playwright() as playwright:
-        return inner_playwrite(playwright, dia_de_reporte, BASE_DIR)
+        return inner_playwrite(playwright, dia_de_reporte, BASE_DIR, logger)
