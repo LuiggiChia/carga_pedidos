23d49ebc619957ee8e1b0f8b07fb18df7c4cff4a
LuiggiChia <167223335+LuiggiChia@users.noreply.github.com>
2026-07-07 16:09:46 -0500

    incluir email v13

--- src/processing/email_from_facturacion.py
+++ src/processing/email_from_facturacion.py
@@ -9,7 +9,13 @@ def email_facturacion(base_dir):
     for file in os.listdir(facturacion_path):
         if file.lower().endswith(".csv"):
             csv_path = os.path.join(facturacion_path, file)
-            dfs.append(pd.read_csv(csv_path, usecols=["ruc", "Textbox3"]))
+            dfs.append(
+                pd.read_csv(
+                    csv_path,
+                    skiprows=2,
+                    usecols=["ruc", "Textbox3"],
+                )
+            )
 
     if not dfs:
         return pd.DataFrame(columns=["Cliente", "E-mail"])
